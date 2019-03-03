5bad206d3e7720adabb930d86e81c679c4f67c4f
Peter Jahn <pejahn@microsoft.com>
2019-03-03 09:59:14 -0800

    Add windows theme to fabric-website-resources

--- apps/fabric-website-resources/src/customizations/customizations.ts
+++ apps/fabric-website-resources/src/customizations/customizations.ts
@@ -1,11 +1,13 @@
 import { IExampleCardCustomizations, IAppCustomizations } from '@uifabric/example-app-base';
 import { FluentCustomizations } from '@uifabric/fluent-theme';
+import { WindowsCustomizations } from '@uifabric/windows-theme';
 import { AzureCustomizationsLight, AzureCustomizationsDark } from '@uifabric/azure-themes';
 import { DefaultCustomizations, TeamsCustomizations, WordCustomizations } from '@uifabric/theme-samples';
 
 const exampleCardCustomizations: IExampleCardCustomizations[] = [
   { title: 'Default', customizations: DefaultCustomizations },
   { title: 'Fluent', customizations: FluentCustomizations },
+  { title: 'Windows', customizations: WindowsCustomizations },
   { title: 'Word', customizations: WordCustomizations },
   { title: 'Teams', customizations: TeamsCustomizations },
   { title: 'Azure', customizations: AzureCustomizationsLight },
